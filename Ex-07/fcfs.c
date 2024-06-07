51817f6018f5ffa036972a753ed633b97410949c
Harshini Akshaya A S <149646981+harshiniakshaya@users.noreply.github.com>
2024-06-07 08:46:56 +0530

    Update fcfs.c

--- Ex-07/fcfs.c
+++ Ex-07/fcfs.c
@@ -14,25 +14,17 @@ int main(){
 		printf("Enter Burst Time:");
 		scanf("%d",&btime[i]);
 	}
-/*
-	int ct[n],cta=0;
-	for(int i=0;i<n;i++){
-		cta+=btime[i];
-		ct[i]=cta;
-	}*/
+
 	int tat[n],temp=0;
 	for(int i=0;i<n;i++){
 		tat[i]=temp+btime[i];
 		temp=tat[i];
 	}
-
-	int bt=0;
-	int bta=0;
+	int tot_wat=0;
 	printf("process\t waiting time\t turn around time\n");
 	for(int i=0;i<n;i++){
-		printf("%s\t\t %d\t\t %d\n",name[i],bt,tat[i]);
-		bt+=btime[i];
-		bta+=bt;
+		printf("%s\t\t %d\t\t %d\n",name[i],tat[i]-btime[i],tat[i]);
+		tot_wat+=tat[i]-btime[i];
 	}
-	printf("The average waiting time: %d\n", bta/n);
+	printf("The average waiting time: %d\n", tot_wat/n);
 }
